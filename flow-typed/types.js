8a37ae57583649a74aa04fdf25977b47a97f071d
mizchi <miz404@gmail.com>
2017-08-03 21:04:11 +0900

    Rewrite with flow

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -1,4 +1,14 @@
 declare module 'sonarish-types' {
+  // Constants
+  declare type $Trivial = 0
+  declare type $Warning = 1
+  declare type $Notable = 2
+  declare type $Error = 3
+  declare type $MustFix = 4
+
+  declare type $Priority = number
+
+  // Internal usage of Eslint
   declare type eslint$CLIEngineResult = {
     errorCount: number,
     warningCount: number,
@@ -13,23 +23,63 @@ declare module 'sonarish-types' {
         line: number,
         column: number
       }>
-    }>,
-  }
-
-  declare type Ruleset = {
-    rulesetName: string,
-    presets?: string[],
-    plugins?: string[],
-    rulesWithPriority: Array<{
-      rule: string,
-      priority?: number,
-      args?: Array<Object>
     }>
   }
 
-  declare type Report = {
-    rulesetName: string,
-    rulesets: Ruleset[],
-    rulesetResult: eslint$CLIEngineResult[]
+  declare type eslint$CLIEngineOptions = {
+    useEslintrc?: boolean,
+    parser?: any,
+    presets?: Array<*>,
+    plugins?: Array<*>,
+    rules?: Object
   }
+
+  // Domain
+  // declare type Ruleset = {
+  //   name: string,
+  //   type: 'eslint', // TODO: Add other types
+  //   // calcScore(result: eslint$CLIEngineResult, ruleset: EslintRulesetInternal): number
+  // }
+
+  declare type RulesetReport = {
+    type: 'eslint',
+    eslintRuleset: EslintRuleset,
+    eslintRawResult: eslint$CLIEngineResult,
+    score: number
+  }
+
+  declare type EslintRuleset = {
+    name: string,
+    type: 'eslint', // TODO: Add other types
+    eslintOptions: eslint$CLIEngineOptions,
+    defaultErrorScore?: number,
+    defaultWarningScore?: number,
+    scoreRules: Array<{
+      rule: string,
+      args?: Array<*>,
+      priority: $Priority
+    }>
+  }
+
+  declare type EslintRulesetInternal = {
+    name: string,
+    eslintOptions: eslint$CLIEngineOptions,
+    defaultErrorScore: number,
+    defaultWarningScore: number,
+    scoreMap: { [rule: string]: $Priority }
+  }
+
+  declare type $buildEslintRuleset = (EslintRuleset) => EslintRulesetInternal
+  declare type $execEslintOnProject = (
+    string,
+    eslint$CLIEngineOptions
+  ) => eslint$CLIEngineResult
+  declare type $calcScore = (
+    result: eslint$CLIEngineResult,
+    scoreMap: { [rule: string]: $Priority }
+  ) => number
+  declare type $report = (
+    projectRootPath: string,
+    opts?: Object
+  ) => RulesetReport[]
 }
--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,43 +1,90 @@
 /* @flow */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/no-unresolved */
 import { CLIEngine } from 'eslint'
-import RULESETS from 'sonarish-ruleset'
-import groupBy from 'lodash.groupby'
+import type {
+  EslintRuleset,
+  $report,
+  $calcScore,
+  $execEslintOnProject,
+  $buildEslintRuleset
+} from 'sonarish-types'
 
 const ERR = 2
 
-const createEngine = (ruleset, priority) => {
-  return new CLIEngine({
-    useEslintrc: false,
-    parser: 'babel-eslint',
-    presets: ruleset.presets,
-    plugins: ruleset.plugins,
-    rules: ruleset.rulesWithPriority
-      .filter(i => i.priority === priority)
-      .reduce((acc, next) => {
-        return Object.assign(acc, {
-          [next.rule]: [ERR].concat(next.args || [])
-        })
+const rulesetList: EslintRuleset[] = [
+  {
+    name: 'code-quality',
+    type: 'eslint',
+    eslintOptions: {
+      useEslintrc: false,
+      parser: 'babel-eslint',
+      plugins: ['mutation', 'eslint-comments']
+    },
+    scoreRules: [
+      {
+        rule: 'prefer-arrow-callback',
+        priority: 1
+      },
+      {
+        rule: 'prefer-const',
+        priority: 2
+      },
+      {
+        rule: 'eslint-comments/no-use',
+        priority: 3
+      },
+      {
+        rule: 'mutation/no-mutation',
+        priority: 0,
+        args: [{ exceptions: ['this', 'that', 'global', 'window', 'module'] }]
+      }
+    ]
+  }
+]
+
+export const buildEslintRuleset: $buildEslintRuleset = ruleset => {
+  return {
+    name: ruleset.name,
+    eslintOptions: {
+      ...ruleset.eslintOptions,
+      useEslintrc: ruleset.eslintOptions.useEslintrc || true,
+      // TODO: Override by defaults
+      rules: ruleset.scoreRules.reduce((acc, i) => {
+        return { ...acc, [i.rule]: [ERR, i.args || []] }
       }, {})
-  })
+    },
+    defaultErrorScore: 1,
+    defaultWarningScore: 3,
+    scoreMap: ruleset.scoreRules.reduce((acc, i) => {
+      return { ...acc, [i.rule]: i.priority }
+    }, {})
+  }
 }
 
-const engines = RULESETS.map(r => ({
-  rulesetName: r.rulesetName,
-  clis: groupBy(
-    [
-      { priority: 0, cli: createEngine(r, 0) },
-      { priority: 1, cli: createEngine(r, 1) },
-      { priority: 2, cli: createEngine(r, 2) },
-      { priority: 3, cli: createEngine(r, 3) }
-    ],
-    i => i.priority
-  )
-}))
+export const execEslintOnProject: $execEslintOnProject = (
+  projectRootPath,
+  opts
+) => new CLIEngine(opts).executeOnFiles([projectRootPath])
 
-export function report(projectPath: string) {
-  return engines.map(e => ({
-    rulesets: RULESETS,
-    rulesetName: e.rulesetName,
-    rulesetResult: e.cliByPriority.map(cli => cli.executeOnFiles([projectPath]))
-  }))
+export const calcScore: $calcScore = (_result, _scoreMap) => {
+  return 0
+}
+
+export const report: $report = (projectRootPath, _opts) => {
+  return rulesetList.map(ruleset => {
+    const rulesetInternal = buildEslintRuleset(ruleset)
+    const eslintRawResult = execEslintOnProject(
+      projectRootPath,
+      rulesetInternal.eslintOptions
+    )
+    const score = calcScore(eslintRawResult, rulesetInternal.scoreMap)
+    return {
+      name: 'code-quality',
+      type: 'eslint',
+      eslintRuleset: ruleset,
+      eslintRawResult,
+      score
+    }
+  })
 }
